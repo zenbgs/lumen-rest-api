9ebd14ddb5e69d0d116234dc570d0295c9c28e43
zenbgs <ancientlord123@gmail.com>
2023-01-18 08:13:37 +0700

    add endpoint biodata

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,24 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->post('/matkul','MatkulController@create');
+$router->post('/matkul', 'MatkulController@create');
 $router->get('/matkul', 'MatkulController@index');
 $router->get('/matkul/{id}', 'MatkulController@show');
 $router->put('/matkul/{id}', 'MatkulController@update');
 $router->delete('/matkul/{id}', 'MatkulController@destroy');
+
+$router->get('/nama', function () {
+    return 'Zain Bagus Dwi Asmoro';
+});
+
+$router->get('/usia', function (){
+    return '21';
+});
+
+$router->get('/alamat', function(){
+    return 'Kota Malang';
+});
+
+$router->get('/profil', function() {
+    return 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur voluptatibus modi nesciunt rem saepe quis nulla sed blanditiis quas molestiae nostrum repellendus natus perferendis, fugiat minima, harum consequuntur, quasi tempore?';
+});
